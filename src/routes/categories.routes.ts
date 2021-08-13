cc953f3fdc38a435fc2e5e21d461ab1a1ec0b2d0
joaovitorpessoa <joaovitorpessoa81@gmail.com>
2021-08-13 17:15:38 -0300

    adicionado rota /categories/import para upload de arquivos com o multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
+import multer from "multer";
+
 import { Router } from "express";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const CategoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 CategoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +16,14 @@ CategoriesRoutes.post("/", (request, response) => {
 
 CategoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+CategoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+
+  console.log(file);
+
+  return response.send();
 });
 
 export { CategoriesRoutes };
