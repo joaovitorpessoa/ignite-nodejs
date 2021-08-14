4cf32e0240773ad42135ab292227ed654a14bd98
João Vitor Pessoa <joaovitorpessoa81@gmail.com>
2021-08-13 23:09:12 -0300

    adicionado swagger e configuração

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express, { json } from "express";
+import swagger from "swagger-ui-express";
+import swaggerConfig from "./swagger.json";
 
 import { routes } from "./routes";
 
 const app = express();
 
 app.use(json());
+
+app.use("/docs", swagger.serve, swagger.setup(swaggerConfig));
+
 app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333! 🚀"));
