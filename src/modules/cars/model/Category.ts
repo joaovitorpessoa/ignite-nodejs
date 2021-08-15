c696c366713557abe4101eeda008fb65ca453c70
João Vitor Pessoa <joaovitorpessoa81@gmail.com>
2021-08-15 16:32:49 -0300

    refatoração no model 'Category' adicionando mapeamento do TypeORM

--- src/modules/cars/model/Category.ts
+++ src/modules/cars/model/Category.ts
@@ -1,16 +1,24 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
+@Entity("categories")
 class Category {
+  @PrimaryColumn()
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   created_at: Date;
 
   constructor({ name, description }) {
     this.id = uuid();
     this.name = name;
     this.description = description;
-    this.created_at = new Date();
   }
 }
 
