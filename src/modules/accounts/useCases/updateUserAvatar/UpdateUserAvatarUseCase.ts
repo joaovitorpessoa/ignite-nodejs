d7a8aeae6728e6141a4b1e9dac93faa57a260835
João Vitor Pessoa <joaovitorpessoa81@gmail.com>
2021-09-07 07:24:38 -0300

    implementado remoção de avatares antigos de usuário no /tmp/avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from "tsyringe";
+
 import { IUsersRepository } from "../../repositories";
+import { deleteFile } from "../../../../utils";
 
 interface IRequest {
   userId: string;
@@ -14,6 +16,10 @@ class UpdateUserAvatarUseCase {
 
   async execute({ userId, avatarFile }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(userId);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
 
     user.avatar = avatarFile;
 
